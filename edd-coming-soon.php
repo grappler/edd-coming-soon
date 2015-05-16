b6c88d694c0125fc00101feb6ca55d5c789a2654
Andrew Munro <andrew@sumobi.com>
2015-05-16 19:35:50 +1200

    formatting

--- edd-coming-soon.php
+++ edd-coming-soon.php
@@ -147,7 +147,7 @@ function edd_coming_soon_admin_price_column( $price, $download_id ) {
 	$votes = get_post_meta($download_id, '_edd_coming_soon_votes');
 	$price .= '<br />' . edd_coming_soon_get_custom_status_text();
 
-	if($cs_active[0] == 1 && ( $votesenabled[0] == 1 || $votes_sc_enabled[0] == 1 ) ) {
+	if ( $cs_active[0] == 1 && ( $votesenabled[0] == 1 || $votes_sc_enabled[0] == 1 ) ) {
 		$price .= '<br /><strong>' . __( 'Votes: ', 'edd-coming-soon' ) . $votes[0] . '</strong>';
 	}
 
